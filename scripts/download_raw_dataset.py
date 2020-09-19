e3d73262f28edef9541bdcd8ae956cdbb4c7fef9
wojtek11530 <wojtek19962a32@gmail.com>
2020-09-19 17:11:17 +0200

    feat: download emoMusic (1000 songs) data

--- scripts/download_raw_dataset.py
+++ scripts/download_raw_dataset.py
@@ -3,6 +3,7 @@ import shutil
 import zipfile
 
 import gdown
+import requests
 
 _PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 _DATASET_PATH = os.path.join(_PROJECT_PATH, 'datasets')
@@ -35,5 +36,27 @@ def download_pmemo_dataset():
     shutil.rmtree(os.path.join(_SCRIPTS_DIRECTORY, pmemo_dataset_name), ignore_errors=True)
 
 
+def download_emomusic_dataset():
+    emomusic_url = 'http://cvml.unige.ch/databases/emoMusic/annotations.tar.gz'
+    emomusic_dataset_name = 'emoMusic'
+    emomusic_output = os.path.join(_SCRIPTS_DIRECTORY, emomusic_dataset_name + '.tar.gz')
+    emomusic_directory = os.path.join(_DATASET_PATH, emomusic_dataset_name)
+    if not os.path.isdir(emomusic_directory):
+        os.mkdir(emomusic_directory)
+
+    r = requests.get(emomusic_url, allow_redirects=True)
+    open(emomusic_output, 'wb').write(r.content)
+
+    shutil.unpack_archive(emomusic_output, os.path.join(_SCRIPTS_DIRECTORY, emomusic_dataset_name))
+    shutil.copy(os.path.join(_SCRIPTS_DIRECTORY, emomusic_dataset_name, 'songs_info.csv'),
+                emomusic_directory)
+    shutil.copy(os.path.join(_SCRIPTS_DIRECTORY, emomusic_dataset_name, 'static_annotations.csv'),
+                emomusic_directory)
+
+    os.remove(emomusic_output)
+    shutil.rmtree(os.path.join(_SCRIPTS_DIRECTORY, emomusic_dataset_name), ignore_errors=True)
+
+
 if __name__ == '__main__':
-    download_pmemo_dataset()
+    # download_pmemo_dataset()
+    download_emomusic_dataset()
