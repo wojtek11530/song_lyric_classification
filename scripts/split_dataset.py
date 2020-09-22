2d33d73bd8a8fecdf032950ea0fe04e224966c82
wojtek11530 <wojtek19962a32@gmail.com>
2020-09-22 14:35:57 +0200

    feat: split the dataset into train, validation and test datasets

---
+++ scripts/split_dataset.py
@@ -0,0 +1,27 @@
+import os
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+_PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+_DATASET_PATH = os.path.join(_PROJECT_PATH, 'datasets')
+_DATA_FILE = 'preprocessed_dataset_with_lyrics.csv'
+
+_RANDOM_SEED = 42
+
+song_df = pd.read_csv(os.path.join(_DATASET_PATH, _DATA_FILE), index_col=0)
+
+df_train, df_test = train_test_split(
+    song_df,
+    test_size=0.2,
+    random_state=_RANDOM_SEED
+)
+df_val, df_test = train_test_split(
+    df_test,
+    test_size=0.5,
+    random_state=_RANDOM_SEED
+)
+
+df_train.to_csv(os.path.join(_DATASET_PATH, 'train_dataset.csv'))
+df_val.to_csv(os.path.join(_DATASET_PATH, 'val_dataset.csv'))
+df_test.to_csv(os.path.join(_DATASET_PATH, 'test_dataset.csv'))
