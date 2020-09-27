298e72a6a0b99e9f96653847d90759781cd80c0d
wojtek11530 <wojtek19962a32@gmail.com>
2020-09-27 12:18:36 +0200

    feat: exclude some stop words

--- preprocessing/text_preprocessor.py
+++ preprocessing/text_preprocessor.py
@@ -4,6 +4,12 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 STOP_WORDS = set(stopwords.words('english'))
+NOT_USED_STOP_WORDS = {'more', 'aren', "mightn't", 'doesn', 'isn', "didn't", 'wouldn', "won't", 'ain', 'couldn',
+                       "shouldn't", "weren't", 'didn', "hadn't", 'needn', 'shouldn', 'mustn', "mustn't", "wasn't",
+                       "couldn't", 'wasn', "hasn't", 'very', 'most', 'hadn', "wouldn't", "don't", "aren't", 'hasn',
+                       "needn't", "haven't", 'nor', 'no', 'won', 'not', 'haven', "isn't", 'don', "doesn't"}
+
+STOP_WORDS = STOP_WORDS - NOT_USED_STOP_WORDS
 
 _TEXT_WITHIN_BRACKETS_REGEX_PATTERN_ = r'[\(\[].*?[\)\]]'
 
