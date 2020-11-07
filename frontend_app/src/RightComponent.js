939ec62902f1b3d204ff4079530c16eefe2f8781
wojtek11530 <wojtek19962a32@gmail.com>
2020-11-07 16:29:09 +0100

    feat(web_frontend): add mock of result presenter

--- frontend_app/src/RightComponent.js
+++ frontend_app/src/RightComponent.js
@@ -3,6 +3,7 @@ import { TextField } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -19,6 +20,18 @@ const useStyles = makeStyles(theme => ({
     },
     fontSize: '1.7rem',
     textTransform: 'none'
+  },
+
+  paper: {
+    backgroundColor: '#cfe8fc',
+    height: '40vh',
+    margin: theme.spacing(1, 1, 1, 1),
+    [theme.breakpoints.up('sm')]: {
+      margin: theme.spacing(4, 2, 1, 2),
+    },
+    [theme.breakpoints.up('md')]: {
+      margin: theme.spacing(4, 4, 1, 4),
+    },
   }
 
 
@@ -29,8 +42,9 @@ const RightComponent = () => {
     return(
       <Container className={classes.container}>
             <Button size="large"  variant="contained"  color="primary" disableElevation className={classes.button}>
-              Get Mood!
+                Get Mood!
             </Button>
+            <Paper className={classes.paper}/>
       </Container>
     )
 }
